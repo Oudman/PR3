d7d98436e435f8a7618a36111b9fe983a511e41e
F.H. Oudman <f.h.oudman@student.tue.nl>
2018-10-23 14:20:53 +0200

    Updated fft tb

--- PR3/fft_tb.sv
+++ PR3/fft_tb.sv
@@ -1,37 +1,60 @@
-`timescale 1ns/1ns
+// -----------------------------------------------------------------------------
+// Copyright (c) 2018 F.H. Oudman
+// -----------------------------------------------------------------------------
+// File:		fft_tb.sv
+// Author:	F.H. Oudman
+// Mail:		f.h.oudman@student.tue.nl
+// -----------------------------------------------------------------------------
+// Type:		testbench
+// Purpose:	testing of fft_int.sv
+// -----------------------------------------------------------------------------
+
+`timescale 1ns/10ps
 
 module fft_tb();
 
 // testing parameters
-localparam FFT_DEPTH = 11;
+localparam FFT_DEPTH = 12;
 localparam SINK_WIDTH = 14;
 localparam FFT_WIDTH = SINK_WIDTH + (FFT_DEPTH+1) / 2;
 
 // Declare inputs as regs and outputs as wires
-bit signed [4:0] cnt = 42;	// 2^5=32 ticks period
-wire [SINK_WIDTH-1:0] sin, time_fft_re;
-bit signed [SINK_WIDTH-1:0] time_fft_im = 0;
-bit clk = 0, reset = 0, time_fft_sop = 0, time_fft_eop = 0, time_fft_valid = 0;
+bit signed [SINK_WIDTH-1:0] time_fft_re, time_fft_im = 0;
+bit clk = 1, reset = 1, time_fft_sop = 0, time_fft_eop = 0, time_fft_valid = 0;
 wire fft_freq_sop, fft_freq_eop, fft_freq_valid, error;
 wire signed [FFT_WIDTH-1:0] fft_peak_re, fft_peak_im;
-shortreal fft_peak_r, fft_peak_theta;
+real fft_peak_r, fft_peak_theta;
+const real					pi = 3.1416;
+real							sin025, sin05, sin075, sin1, sin2, sin3, sin4, sin5, sin6, sin7, sin8;
 
 // clock generator(s)
-always #(25ns) clk++; // F = 20.0MHz
+always #(25441ps) clk++;	// Fs = 20.48 MHz
 
 // sine approx generator
-always #(10ns) cnt++; // F = 100MHz / 32 = 3.13MHz = 15.6% of 20MHz
-assign sin = (cnt < 0) ? (512 * cnt + 32 * cnt * cnt) : (512 * cnt - 32 * cnt * cnt);
-assign time_fft_re = sin;
+always @(negedge clk)
+begin
+	sin025 = 1024 * $sin(2 * pi * 25E4 * $time / 1E9);
+	sin05  = 1024 * $sin(2 * pi * 50E4 * $time / 1E9);
+	sin075 = 1024 * $sin(2 * pi * 75E4 * $time / 1E9);
+	sin1 = 1024 * $sin(2 * pi * 1.0E6 * $time / 1E9);
+	sin2 = 1024 * $sin(2 * pi * 2.0E6 * $time / 1E9);
+	sin3 = 1024 * $sin(2 * pi * 3.0E6 * $time / 1E9);
+	sin4 = 1024 * $sin(2 * pi * 4.0E6 * $time / 1E9);
+	sin5 = 1024 * $sin(2 * pi * 5.0E6 * $time / 1E9);
+	sin6 = 1024 * $sin(2 * pi * 6.0E6 * $time / 1E9);
+	sin7 = 1024 * $sin(2 * pi * 7.0E6 * $time / 1E9);
+	sin8 = 1024 * $sin(2 * pi * 8.0E6 * $time / 1E9);
+end
 
-assign fft_peak_r = $hypot(shortreal'(fft_peak_re), shortreal'(fft_peak_im));
+assign time_fft_re = sin025 + sin05 + sin075 + sin1 + sin2 + sin3 + sin4 + sin5 + sin6 + sin7 + sin8;
+
+assign fft_peak_r = $hypot(fft_peak_re, fft_peak_im);
 assign fft_peak_theta = $atan2(fft_peak_im, fft_peak_re);
 
 initial
 begin
-	#(100ns) reset = 1;
 	#(100ns) reset = 0;
-	#(100ns) time_fft_valid = 1;
+	time_fft_valid = 1;
 	forever
 	begin
 		time_fft_sop = 1;
